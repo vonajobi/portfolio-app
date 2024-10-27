66f351589a9f695f1f50ad272a8db796b6c88838
vonajobi <130017130+vonajobi@users.noreply.github.com>
2024-10-26 19:14:11 -0700

    updated rootlayout metadata
    removed title props from rootlayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,25 +5,20 @@ import { Navbar } from './components/dom/navBar'
 import { Footer } from './components/dom/footer'
 import Header from './components/dom/header';
 import React from 'react';
+import { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function RootLayout({
-  children,
-  title,
-}: {
-  children: React.ReactNode;
-  title: string;
-}) {
+export default function RootLayout({children}: {children: React.ReactNode}) {
 
   return (
     <html lang="en">
-      <Header title={title}/>
+      <Header/>
       <body>
         <div className={`${inter.className} flex flex-col min-h-screen`}>
           <Navbar />
           <main className="flex-grow flex pt-20">
-            {React.cloneElement(children as React.ReactElement, { title })}
+            {children}
           </main>
           <Footer />
         </div>
